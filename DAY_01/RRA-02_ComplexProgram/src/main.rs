5b2e6d21a90c54a746be939f11812b5ab3d0fed5
vipin09 <sainivipin1998@gmail.com>
2022-08-31 00:07:37 +0530

    packages

--- DAY_01/RRA-02_ComplexProgram/src/main.rs
+++ DAY_01/RRA-02_ComplexProgram/src/main.rs
@@ -2,9 +2,11 @@ pub mod encrypter;
 use std::io;
 use encrypter::Encryptable;
 fn main() {
+    loop{
     println!("Input the String you want to encrypt");
     let mut user_input=String::new();
 
     io::stdin().read_line(&mut user_input).expect("cannot read input");
     println!("Your encrypted String : {}",  encrypter::rot13::Rot13(user_input).encrypt());
+}
 }
